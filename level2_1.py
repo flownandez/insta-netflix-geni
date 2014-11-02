2eafbb4788abb55f46a782e44fe00a75b24a974d
flownandez <chris2fernandez@gmail.com>
2014-11-02 15:39:17 -0600

    Create and configue level2_1 prototype.

---
+++ level2_1.py
@@ -0,0 +1,89 @@
+import socket
+import sys
+ 
+HOSTrecv1 = '10.10.0.0'   # Symbolic name meaning all available interfaces
+PORTrecv1 = 8887 # Arbitrary non-privileged port
+HOSTrecv2 = '10.10.1.0'   # Symbolic name meaning all available interfaces
+PORTrecv2 = 8888 # Arbitrary non-privileged port
+HOSTsend = '10.10.4.1'
+PORTsend = 8889
+ 
+############### Datagram (udp) RECEV1 socket ###############
+try :
+    srecev1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print 'Socket created'
+except socket.error, msg :
+    print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+# Bind socket to local host and port
+try:
+    srecev1.bind((HOSTrecv1, PORTrecv1))
+except socket.error , msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+print 'Socket bind complete'
+
+############### Datagram (udp) RECEV2 socket ###############
+try :
+    srecev2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print 'Socket created'
+except socket.error, msg :
+    print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+# Bind socket to local host and port
+try:
+    srecev2.bind((HOSTrecv2, PORTrecv2))
+except socket.error , msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+print 'Socket bind complete'
+
+############### Datagram (udp) SEND socket ###############
+try :
+    ssend = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print 'Socket created'
+except socket.error, msg :
+    print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+# Bind socket to local host and port
+try:
+    ssend.bind((HOSTsend, PORTsend))
+except socket.error , msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+print 'Socket bind complete'
+ 
+############### now keep talking with the client ###############
+while 1:
+    # receive data from client (data, addr)
+    d1 = srecev1.recvfrom(1024)
+    data1 = d1[0]
+    addr1 = d1[1]
+     
+    if not data1: 
+        break
+     
+    reply1 = 'OK...' + data1
+     
+    srecev1.sendto(reply1 , addr1)
+    print 'Message[' + addr1[0] + ':' + str(addr1[1]) + '] - ' + data1.strip()
+
+        # receive data from client (data, addr)
+    d2 = srecev2.recvfrom(1024)
+    data2 = d2[0]
+    addr2 = d2[1]
+     
+    if not data2: 
+        break
+     
+    reply2 = 'OK...' + data2
+     
+    srecev2.sendto(reply2 , addr2)
+    print 'Message[' + addr2[0] + ':' + str(addr2[1]) + '] - ' + data2.strip()
+
+    # send data from clients (data, addr) to layer 3
+    newdata = d1[0] + ' ' + d2[0] 
+    newaddr = (HOSTsend, PORTsend)
+          
+    ssend.sendto(newdata , newaddr)     
+#s.close(
