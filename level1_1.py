399ee63135e5bcc1fb7f090538227c1774148bcc
flownandez <chris2fernandez@gmail.com>
2014-11-02 15:49:30 -0600

    Add level3_test and config

---
+++ level1_1.py
@@ -0,0 +1,31 @@
+import socket   #for sockets
+import sys  #for exit
+ 
+# create dgram udp socket
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except socket.error:
+    print 'Failed to create socket'
+    sys.exit()
+ 
+host = '10.10.0.1';
+port = 8887;
+ 
+while(1) :
+    msg = 1
+     
+    try :
+        #Set the whole string
+        s.sendto(msg, (host, port))
+         
+        # receive data from client (data, addr)
+        d = s.recvfrom(1024)
+        reply = d[0]
+        addr = d[1]
+         
+        print 'Server reply : ' + reply
+        msg = msg + 1
+     
+    except socket.error, msg:
+        print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        sys.exit()
