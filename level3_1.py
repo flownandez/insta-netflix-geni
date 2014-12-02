6cd5f05ebb943b88702265012ed8687fd78fc20f
Nick Levin <Nick@Nicks-MacBook-Pro-2.local>
2014-12-02 00:55:43 -0600

    Changed a shit ton in level3

--- level3_1.py
+++ level3_1.py
@@ -62,29 +62,52 @@ except socket.error:
     sys.exit()
 
 startTime = time.time();
+oneSecond = 0;
+reply2_1, reply2_2 = "START"
 pps1, pps2, pps3, pps4 = 0;
+packetsReceived = [];
+for i in range(30100) :
+	packetsReceived.append(0) ;
+
 ############### now keep talking with the client ###############
 while 1:
 	newTime = time.time();
 	if((newTime - startTime) >= 1) : 
 		startTime = time.time();
-	
+		oneSecond = 1;
+		
 		#this part finds the lowest packets per second of any level1 node
 		lowest = min(pps1, pps2, pps3, pps4);
-
+		
 		#this is where the number of tokens each level1 node gets are assigned
-		if(pps1 < (lowest + 5)) tokensToAllocate1 = 1;	 #if pps1 is close to lowest, give it 1 token
-		else tokensToAllocate1 = int((pps1 + 5) / lowest);  #else give it tokens proportional to its pps compared to lowest
-		                                           # can hardcode to 2 if issues
+		if(pps1 == 0): 
+			tokensToAllocate1 = 0; 		
+		elif(pps1 < (lowest + 5)) :
+			tokensToAllocate1 = 1;	 #if pps1 is close to lowest, give it 1 token
+		else :
+			tokensToAllocate1 = int((pps1 + 5) / (lowest + 1));  #else give it tokens proportional to its pps compared to lowest
+		                                           				# can hardcode to 2 if issues
 
-		if(pps2 < (lowest + 5)) tokensToAllocate2 = 1; #do the same for all nodes
-		else tokensToAllocate2 = int((pps2 + 5) / lowest); 
+		if(pps2 == 0) :
+			tokensToAllocate2 = 0;	
+		elif(pps2 < (lowest + 5)):
+			tokensToAllocate2 = 1; #do the same for all nodes
+		else :
+			tokensToAllocate2 = int((pps2 + 5) / (lowest + 1)); 
 
-		if(pps3 < (lowest + 5)) tokensToAllocate3 = 1;
-		else tokensToAllocate3 = int((pps3 + 5) / lowest);
+		if(pps3 == 0):
+			tokensToAllocate3 = 0; 	
+		elif(pps3 < (lowest + 5)):
+			tokensToAllocate3 = 1;
+		else:
+			tokensToAllocate3 = int((pps3 + 5) / (lowest + 1));
 
-		if(pps4 < (lowest + 5)) tokensToAllocate4 = 1;
-		else tokensToAllocate4 = int((pps4 + 5) / lowest);
+		if(pps4 == 0):
+			tokensToAllocate4 = 0; 	
+		elif(pps4 < (lowest + 5)):
+			tokensToAllocate4 = 1;
+		else:
+			tokensToAllocate4 = int((pps4 + 5) / (lowest + 1));
 
 		totalTokens = tokensToAllocate1 + tokensToAllocate2 + tokensToAllocate3 + tokensToAllocate4;
 
@@ -98,37 +121,32 @@ while 1:
 		#these variables will keep track of how many tokens each node has been assigned
 		tokensAllocated1, tokensAllocated2, tokensAllocated3, tokensAllocated4 = 0;
 
-		while(tokenNumber < totalTokens) #perform until all tokens are allocated
-		{
-			if(tokensAllocated1 < tokensToAllocate1 && tokenNumber < totalTokens)
-			{
-				message1 = message1 + " " + tokenNumber; #allocate token by adding token number to message
-				tokenNumber = tokenNumber + 1; #increment token number
-				tokensAllocated1 = tokensAllocated1 + 1; #node 1 has been assigned 1 more token
-			}
+		while tokenNumber < totalTokens : #perform until all tokens are allocated
+			if(tokensAllocated1 < tokensToAllocate1):
+				if (tokenNumber < totalTokens) :
+					message1 = message1 + " " + tokenNumber; #allocate token by adding token number to message
+					tokenNumber = tokenNumber + 1; #increment token number
+					tokensAllocated1 = tokensAllocated1 + 1; #node 1 has been assigned 1 more token
 
-			if(tokensAllocated2 < tokensToAllocate2 && tokenNumber < totalTokens)
-			{
-				message2 = message2 + " " + tokenNumber;
-				tokenNumber = tokenNumber + 1; #increment token number
-				tokensAllocated2 = tokensAllocated2 + 1;
-			}
+			if(tokensAllocated2 < tokensToAllocate2):
+				if(tokenNumber < totalTokens) :
+					message2 = message2 + " " + tokenNumber;
+					tokenNumber = tokenNumber + 1; #increment token number
+					tokensAllocated2 = tokensAllocated2 + 1;
 
-			if(tokensAllocated3 < tokensToAllocate3 && tokenNumber < totalTokens)
-			{
-				message3 = message3 + " " + tokenNumber;
-				tokenNumber = tokenNumber + 1; #increment token number
-				tokensAllocated3 = tokensAllocated3 + 1;
-			}
+			if(tokensAllocated3 < tokensToAllocate3):
+				if(tokenNumber < totalTokens) :
+					message3 = message3 + " " + tokenNumber;
+					tokenNumber = tokenNumber + 1; #increment token number
+					tokensAllocated3 = tokensAllocated3 + 1;
 
-			if(tokensAllocated4 < tokensToAllocate4 && tokenNumber < totalTokens)
-			{
-				message4 = message4 + " " + tokenNumber;
-				tokenNumber = tokenNumber + 1; #increment token number
-				tokensAllocated4 = tokensAllocated4 + 1;
-			}
-		}
+			if(tokensAllocated4 < tokensToAllocate4) :
+				if(tokenNumber < totalTokens) :
+					message4 = message4 + " " + tokenNumber;
+					tokenNumber = tokenNumber + 1; #increment token number
+					tokensAllocated4 = tokensAllocated4 + 1;
 
+		startingPkt = packetsReceived.index(0); 
 		message1 = message1 + " X " + startingPkt; #message format will be "totalTokens tokens[..] X startingPkt"
 		message2 = message2 + " X " + startingPkt;
 		message3 = message3 + " X " + startingPkt;
@@ -136,6 +154,7 @@ while 1:
 
 		reply2_1 = message1 + " " + message2;
 		reply2_2 = message3 + " " + message4;
+		pps1, pps2, pps3, pps4 = 0;
 
 	readySockets, blank1, blank2 = select.select([srecev1, srecev2], [], []);
 	for sock in readySockets:
@@ -144,9 +163,14 @@ while 1:
     		addr = d[1]
 		if not data: 
         		break
-	 
-		reply = 'OK: ' + data	 
-		sock.sendto(reply , addr)
+
+		if(addr == HOSTrecv1) :
+			reply = reply2_1;
+		else :
+			reply = reply2_2; 
+		
+		sock.sendto(reply , addr) 
+		
 		#print 'Message[' + addr1[0] + ':' + str(addr1[1]) + '] - ' + data1.strip()
 		datatmp = data.split()
 		print(datatmp[0] + " " + datatmp[1])
@@ -158,7 +182,10 @@ while 1:
 			pps3 = pps3 + 1;
 		if(int(datatmp[1]) == 4) :
 			pps4 = pps4 + 1;
-
+		
+		packetsReceived[int(datatmp[0])] = 1;  
+		if(int(datatmp[0]) > 5000) :
+			s.close();
 		# send data from clients (data, addr) to layer 4
 		newdata = d[0] 
 		newaddr = (HOSTsend, PORTsend)
