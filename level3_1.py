d82b48c67b7c3cd7fca695d80ddc43da9ba078a8
Nick Levin <Nick@dyn-72-33-198-1.uwnet.wisc.edu>
2014-12-02 15:38:54 -0600

    ready for iperf

--- level3_1.py
+++ level3_1.py
@@ -83,7 +83,7 @@ while 1:
 		#this part finds the lowest packets per second of any level1 node
 		lowest = min(pps1, pps2, pps3, pps4);
 		
-		#print "pps1: ------  " + str(pps1)		
+		print "pps1: ------  " + str(pps1)		
 		#print "pps2: ------  " + str(pps2)
 		#print "pps3: ------  " + str(pps3)
 		#print "pps4: ------  " + str(pps4)		
