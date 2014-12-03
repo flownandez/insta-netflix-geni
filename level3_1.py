dd258fb8ab66dbd13215ba21639d161b597770b8
Nick Levin <Nick@dyn-72-33-198-1.uwnet.wisc.edu>
2014-12-02 22:12:15 -0600

    Scenario C 4

--- level3_1.py
+++ level3_1.py
@@ -118,7 +118,7 @@ while 1:
 			elif(pps1 < (lowest + 5)) :
 				tokensToAllocate1 = 1;	 #if pps1 is close to lowest, give it 1 token
 			else : 			
-				tokensToAllocate1 = int((pps1 + 5) / lowest);  #else give it tokens proportional to its pps compared to lowest
+				tokensToAllocate1 = int((pps1 + 12) / lowest);  #else give it tokens proportional to its pps compared to lowest
 				                                   				# can hardcode to 2 if issues
 
 			if(pps2 == 0) :
@@ -126,21 +126,21 @@ while 1:
 			elif(pps2 < (lowest + 5)):
 				tokensToAllocate2 = 1; #do the same for all nodes
 			else :
-				tokensToAllocate2 = int((pps2 + 5) / lowest); 
+				tokensToAllocate2 = int((pps2 + 12) / lowest); 
 
 			if(pps3 == 0):
 				tokensToAllocate3 = 0; 	
 			elif(pps3 < (lowest + 5)):
 				tokensToAllocate3 = 1;
 			else:
-				tokensToAllocate3 = int((pps3 + 5) / lowest);
+				tokensToAllocate3 = int((pps3 + 12) / lowest);
 
 			if(pps4 == 0):
 				tokensToAllocate4 = 0; 	
 			elif(pps4 < (lowest + 5)):
 				tokensToAllocate4 = 1;
 			else:
-				tokensToAllocate4 = int((pps4 + 5) / lowest);
+				tokensToAllocate4 = int((pps4 + 12) / lowest);
 
 		totalTokens = tokensToAllocate1 + tokensToAllocate2 + tokensToAllocate3 + tokensToAllocate4;
 
