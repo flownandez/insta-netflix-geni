218ed1817745d518c030885519c78042953ef2bc
Nick Levin <Nick@dyn-72-33-198-1.uwnet.wisc.edu>
2014-12-02 22:37:49 -0600

    Scenario D 2

--- level4.py
+++ level4.py
@@ -25,7 +25,7 @@ print 'Socket bind complete'
  
 firstround = 1
 transtime = 0
-file = open("ScenarioD_Level1_2_Level2_1.txt", "w")
+file = open("ScenarioD_Level1_3_Level2_1.txt", "w")
 #now keep talking with the client
 while 1:
     # receive data from client (data, addr)
