0a41938cff036279f2adae211149599a847d5469
Nick Levin <Nick@dyn-72-33-198-1.uwnet.wisc.edu>
2014-12-02 20:58:05 -0600

    Scenario C 2

--- level3_1.py
+++ level3_1.py
@@ -71,6 +71,7 @@ pps2 = 0
 pps3 = 0
 pps4 = 0
 newDataPkt = 0
+seconds = 0;
 packetsReceived = [];
 previousMessages = [" ", " ", " ", " "];
 for i in range(30101) :
@@ -82,14 +83,14 @@ while 1:
 	if((newTime - prevTime) >= 1) : 
 		prevTime = time.time();
 		oneSecond = 1;
-							
+		seconds = seconds + 1; 					
 		#this part finds the lowest packets per second of any level1 node
 		lowest = min(pps1, pps2, pps3, pps4);
 		
-		print "pps1: ------  " + str(pps1)		
-		print "pps2: ------  " + str(pps2)
-		print "pps3: ------  " + str(pps3)
-		print "pps4: ------  " + str(pps4)		
+		#print "pps1: ------  " + str(pps1)		
+		#print "pps2: ------  " + str(pps2)
+		#print "pps3: ------  " + str(pps3)
+		#print "pps4: ------  " + str(pps4)		
 
 		
 		#this is where the number of tokens each level1 node gets are assigned
@@ -195,27 +196,27 @@ while 1:
 		if (tokensAllocated4 == 0):
 			message4 = message4 + " 0";
 		
-		#if previousMessages == [message1, message2, message3, message4] :
-		#	previousMessages = [message1, message2, message3, message4];
-		#	if(newDataPkt > (startingPkt + 100)) :
-		#		print 'New Message'
-		#		previousMessages = [message1, message2, message3, message4];
-		#		message1 = message1 + " X " + str(startingPkt); #message format will be "totalTokens tokens[..] X startingPkt"
-		#		message2 = message2 + " Y " + str(startingPkt);
-		#		message3 = message3 + " X " + str(startingPkt);
-		#		message4 = message4 + " Y " + str(startingPkt);
-		#		reply2_1 = message1 + " B " + message2;
-		#		reply2_2 = message3 + " B " + message4;
+		if previousMessages == [message1, message2, message3, message4] :
+			previousMessages = [message1, message2, message3, message4];
+			if(seconds == 10) :
+				seconds = 0;
+				previousMessages = [message1, message2, message3, message4];
+				message1 = message1 + " X " + str(startingPkt); #message format will be "totalTokens tokens[..] X startingPkt"
+				message2 = message2 + " Y " + str(startingPkt);
+				message3 = message3 + " X " + str(startingPkt);
+				message4 = message4 + " Y " + str(startingPkt);
+				reply2_1 = message1 + " B " + message2;
+				reply2_2 = message3 + " B " + message4;
 			 
-		#else :
-		#print 'New Message'
-		#previousMessages = [message1, message2, message3, message4];
-		message1 = message1 + " X " + str(startingPkt); #message format will be "totalTokens tokens[..] X startingPkt"
-		message2 = message2 + " Y " + str(startingPkt);
-		message3 = message3 + " X " + str(startingPkt);
-		message4 = message4 + " Y " + str(startingPkt);
-		reply2_1 = message1 + " B " + message2;
-		reply2_2 = message3 + " B " + message4;
+		else :
+			#print 'New Message'
+			previousMessages = [message1, message2, message3, message4];
+			message1 = message1 + " X " + str(startingPkt); #message format will be "totalTokens tokens[..] X startingPkt"
+			message2 = message2 + " Y " + str(startingPkt);
+			message3 = message3 + " X " + str(startingPkt);
+			message4 = message4 + " Y " + str(startingPkt);
+			reply2_1 = message1 + " B " + message2;
+			reply2_2 = message3 + " B " + message4;
 
 		pps1 = 0 #reset counters
 		pps2 = 0 
